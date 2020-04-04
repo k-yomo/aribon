88eed332c163b812027f46be5cbad9f291476952
k-yomo <kanji.yy@gmail.com>
2020-04-05 00:31:48 +0900

    Solve 1-6 ants

---
+++ 1_6/ants/main.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	io, flush := NewIO()
+	defer flush()
+	l, n := io.ScanInt(), io.ScanInt()
+	ants := io.ScanInts(n)
+	sort.Ints(ants)
+	max := Max(l-ants[0], ants[n-1])
+	min := 0
+	for _, a := range ants {
+		if Min(a, l-a) > min {
+			min = Min(a, l-a)
+		}
+	}
+	fmt.Println(min, max)
+}
+
+func AbsDiff(n1 int, n2 int) int {
+	return int(math.Abs(float64(n1 - n2)))
+}
+
+func Min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func Max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+type IO struct {
+	scanner *bufio.Scanner
+	writer  *bufio.Writer
+}
+
+func NewIO() (*IO, func()) {
+	io := &IO{
+		scanner: newScanner(),
+		writer:  newWriter(),
+	}
+	return io, func() { io.writer.Flush() }
+}
+
+func newScanner() *bufio.Scanner {
+	s := bufio.NewScanner(os.Stdin)
+	s.Buffer(make([]byte, 10000000), 10000000)
+	s.Split(bufio.ScanWords)
+	return s
+}
+
+func newWriter() *bufio.Writer {
+	return bufio.NewWriter(os.Stdout)
+}
+
+func (io *IO) ScanBytes() []byte {
+	if !io.scanner.Scan() {
+		panic("scan string failed")
+	}
+	return io.scanner.Bytes()
+}
+
+func (io *IO) ScanString() string {
+	if !io.scanner.Scan() {
+		panic("scan string failed")
+	}
+	return io.scanner.Text()
+}
+
+func (io *IO) ScanStrings(n int) []string {
+	strs := make([]string, n)
+	for i := 0; i < n; i++ {
+		strs[i] = io.ScanString()
+	}
+	return strs
+}
+
+func (io *IO) Scan2DStrings(y, x int) [][]string {
+	strings := make([][]string, y)
+	for i := 0; i < y; i++ {
+		strings[i] = io.ScanStrings(x)
+	}
+	return strings
+}
+
+func (io *IO) Scan2DGraph(y int) [][]string {
+	strs := make([][]string, y)
+	for i := 0; i < y; i++ {
+		strs[i] = strings.Split(io.ScanString(), "")
+	}
+	return strs
+}
+
+func (io *IO) ScanInt() int {
+	return int(io.ScanInt64())
+}
+
+func (io *IO) ScanInts(n int) []int {
+	ints := make([]int, n)
+	for i := 0; i < n; i++ {
+		ints[i] = io.ScanInt()
+	}
+	return ints
+}
+
+func (io *IO) Scan2DInts(y, x int) [][]int {
+	ints := make([][]int, y)
+	for i := 0; i < y; i++ {
+		ints[i] = io.ScanInts(x)
+	}
+	return ints
+}
+
+func (io *IO) ScanInt64() int64 {
+	i, err := strconv.ParseInt(io.ScanString(), 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func (io *IO) ScanFloat64() float64 {
+	i, _ := strconv.ParseFloat(io.ScanString(), 64)
+	return i
+}
+
+func (io *IO) ScanFloat64s(n int) []float64 {
+	floats := make([]float64, n)
+	for i := 0; i < n; i++ {
+		floats[i] = io.ScanFloat64()
+	}
+	return floats
+}
+
+func (io *IO) Println(a ...interface{}) {
+	fmt.Fprintln(io.writer, a...)
+}
